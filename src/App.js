6df6e7a42db9daa40e283e934f2c4c22ed9ca21f
co8mcate <37420994+co8mcate@users.noreply.github.com>
2019-11-25 08:04:49 -0800

    added a sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
-import Navbar from './Components/Navbar';
-import Activities from './Components/Activities';
-import Schedule from './Components/Schedule';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Sidebar from "./Components/Sidebar";
+import Navbar from "./Components/Navbar";
+import Activities from "./Components/Activities";
+import Schedule from "./Components/Schedule";
 //import Messages from './Messages';
 //import Schedule from './Schedule';
 //import Tutors from './Tutors';
@@ -16,27 +12,28 @@ import Schedule from './Components/Schedule';
 function App() {
   return (
     <Router>
-    <div className="App">
-      <header className="App-header">
-      <h1> MentorBoard</h1>
-      <Schedule></Schedule>
-      <Switch>
-        <Route exact path="/home" component={Navbar}/>
-        </Switch>
-      </header>
-      <div className = "App-body">
-        <Switch>
-        <Route path="/activities" component={Activities}/>  
-        </Switch>
+      <div className="App">
+        <header className="App-header">
+          <h1> MentorBoard</h1>
+          <Sidebar></Sidebar>
+          <Schedule></Schedule>
+          <Switch>
+            <Route exact path="/home" component={Navbar} />
+          </Switch>
+        </header>
+        <div className="App-body">
+          <Switch>
+            <Route path="/activities" component={Activities} />
+          </Switch>
+        </div>
+        <div>
+          <Switch>
+            <Route path="/schedule" component={Schedule} />
+          </Switch>
+        </div>
       </div>
-      <div>
-        <Switch>
-        <Route path="/schedule" component={Schedule}/>  
-        </Switch>
-      </div>
-    </div>
     </Router>
   );
-  }
+}
 
 export default App;
