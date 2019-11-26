35d9540989399a2094945567df8b09b1008eee24
co8mcate <37420994+co8mcate@users.noreply.github.com>
2019-11-26 09:49:39 -0800

    shift the button

--- src/App.js
+++ src/App.js
@@ -12,26 +12,24 @@ import Schedule from "./Components/Schedule";
 function App() {
   return (
     <Router>
+      <Sidebar></Sidebar>
       <div className="App">
-        <Sidebar></Sidebar>
+        <header className="App-header">
+          <h1> MentorBoard</h1>
+          <Schedule></Schedule>
+          <Switch>
+            <Route exact path="/home" component={Navbar} />
+          </Switch>
+        </header>
+        <div className="App-body">
+          <Switch>
+            <Route path="/activities" component={Activities} />
+          </Switch>
+        </div>
         <div>
-          <header className="App-header">
-            <h1> MentorBoard</h1>
-            <Schedule></Schedule>
-            <Switch>
-              <Route exact path="/home" component={Navbar} />
-            </Switch>
-          </header>
-          <div className="App-body">
-            <Switch>
-              <Route path="/activities" component={Activities} />
-            </Switch>
-          </div>
-          <div>
-            <Switch>
-              <Route path="/schedule" component={Schedule} />
-            </Switch>
-          </div>
+          <Switch>
+            <Route path="/schedule" component={Schedule} />
+          </Switch>
         </div>
       </div>
     </Router>
