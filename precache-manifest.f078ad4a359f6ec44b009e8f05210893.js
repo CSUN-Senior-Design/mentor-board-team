8b3d2309ff8ba080a6d693174423114ae26efe1b
umarnasir335 <umarnasir335@gmail.com>
2019-12-01 23:15:02 -0800

    Updates

---
+++ precache-manifest.f078ad4a359f6ec44b009e8f05210893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e939b83c2a9dbcc03afc315a86436dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a212c88f939086aeed",
+    "url": "/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "16529888c6dafd2005fc",
+    "url": "/static/css/main.4fa231ad.chunk.css"
+  },
+  {
+    "revision": "55a212c88f939086aeed",
+    "url": "/static/js/2.488f4e3a.chunk.js"
+  },
+  {
+    "revision": "16529888c6dafd2005fc",
+    "url": "/static/js/main.8107d5db.chunk.js"
+  },
+  {
+    "revision": "ad39a0480ba1a55de8f5",
+    "url": "/static/js/runtime-main.98019822.js"
+  }
+]);
