a102d338d5f5411bf389be0baf8bcfe9c1454a92
haemin9014 <leehaemin9014@gmail.com>
2020-01-30 10:27:14 -0800

    working on profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
+import React, {useState} from 'react'
 import Courses from './courses/Courses'
 import Navbar from './Navbar'
 
 export class Profile extends Component {
     render() {
+        const [image, setImage] = useState('')
+        const [loading, setLoading] = useState(false)
+        const uploadImage = async e => {
+            const file = e.target.files
+            const data = new FormData()
+            data.append('file', files[0])
+            data.append('upload_present', )
+        }
         return (
-            <div >
-               <div className= "App-body">
-               <Courses />
-               </div>
+            <div className = "profile_image">
+             <input type ="file"
+                name="file"
+                placeholder ="upload"
+                onChange = [uploadImage]
+            />   
             </div>
         )
     }
