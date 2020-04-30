8ae78ce2c7d172cdf34e1888f042428a505d259f
jsc31994 <jsc31994@gmail.com>
2020-04-30 00:29:07 -0700

    Implemented search feature on tutors page

--- src/Components/Tutors.js
+++ src/Components/Tutors.js
@@ -7,43 +7,53 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
+import TutorsList from "./tutors/TutorsList";
 
 export class Tutors extends Component {
-   state = {
-      tutorCards: [
-         {
-            key: "1",
-            inst_name: "Jaime Escalante",
-            inst_bio:
-               "Jaime Alfonso Escalante Gutiérrez was a Bolivian-American educator known for teaching students calculus from 1974 to 1991 at Garfield High School in East Los Angeles.",
-            inst_pic:
-               "https://www.biography.com/.image/t_share/MTE5NDg0MDU0NzE1NzI5NDIz/jaime-escalante-189368-1-402.jpg",
-         },
-         {
-            key: "2",
-            inst_name: "Kyle Parker",
-            inst_bio:
-               "A stereotypical laid back hippie surfer who tells nonsense stories, such as describing the time he lost a cordless phone, or about how finding a dead bird made him change his normal route to school.",
-            inst_pic:
-               "https://vignette.wikia.nocookie.net/nickelodeon/images/f/f6/Totally_Kyle.png/revision/latest/scale-to-width-down/340?cb=20170425151239",
-         },
-         {
-            key: "3",
-            inst_name: "Rick Sanchez",
-            inst_bio:
-               "A genius scientist whose alcoholism and reckless, nihilistic behavior are a source of concern for his daughter's family, as well as the safety of their son, Morty.",
-            inst_pic:
-               "https://i.pinimg.com/originals/6e/51/32/6e5132a90812ad1abf3711135a5cf406.png",
-         },
-         {
-            key: "4",
-            inst_name: "Walter White",
-            inst_bio:
-               "A graduate of the California Institute of Technology (Caltech), Walter co-founded the company Gray Matter Technologies with his close friend Elliott Schwartz and his then-girlfriend Gretchen. He left Gray Matter abruptly, selling his shares for $5,000.",
-            inst_pic:
-               "https://metro.co.uk/wp-content/uploads/2017/07/walter-white-breaking-bad.jpg?quality=90&strip=all",
-         },
-      ],
+   constructor() {
+      super();
+      this.state = {
+         searchField: "",
+         tutorCards: [
+            {
+               key: "1",
+               inst_name: "Jaime Escalante",
+               inst_bio:
+                  "Jaime Alfonso Escalante Gutiérrez was a Bolivian-American educator known for teaching students calculus from 1974 to 1991 at Garfield High School in East Los Angeles.",
+               inst_pic:
+                  "https://www.biography.com/.image/t_share/MTE5NDg0MDU0NzE1NzI5NDIz/jaime-escalante-189368-1-402.jpg",
+            },
+            {
+               key: "2",
+               inst_name: "Kyle Parker",
+               inst_bio:
+                  "A stereotypical laid back hippie surfer who tells nonsense stories, such as describing the time he lost a cordless phone, or about how finding a dead bird made him change his normal route to school.",
+               inst_pic:
+                  "https://vignette.wikia.nocookie.net/nickelodeon/images/f/f6/Totally_Kyle.png/revision/latest/scale-to-width-down/340?cb=20170425151239",
+            },
+            {
+               key: "3",
+               inst_name: "Rick Sanchez",
+               inst_bio:
+                  "A genius scientist whose alcoholism and reckless, nihilistic behavior are a source of concern for his daughter's family, as well as the safety of their son, Morty.",
+               inst_pic:
+                  "https://i.pinimg.com/originals/6e/51/32/6e5132a90812ad1abf3711135a5cf406.png",
+            },
+            {
+               key: "4",
+               inst_name: "Walter White",
+               inst_bio:
+                  "A graduate of the California Institute of Technology (Caltech), Walter co-founded the company Gray Matter Technologies with his close friend Elliott Schwartz and his then-girlfriend Gretchen. He left Gray Matter abruptly, selling his shares for $5,000.",
+               inst_pic:
+                  "https://metro.co.uk/wp-content/uploads/2017/07/walter-white-breaking-bad.jpg?quality=90&strip=all",
+            },
+         ],
+      };
+   }
+
+   handleChange = (e) => {
+      console.log("handleChange");
+      this.setState({ searchField: e.target.value });
    };
 
    render() {
@@ -58,21 +68,23 @@ export class Tutors extends Component {
          },
       }));
 
+      const filteredTutors = this.state.tutorCards.filter((tutor) =>
+         tutor.inst_name
+            .toLowerCase()
+            .includes(this.state.searchField.toLowerCase())
+      );
+
       return (
          <React.Fragment>
-            <div>
-               <Header />
-               <RecommendedClasses />
+            <Header />
+            <RecommendedClasses />
 
-               <TutorSearchBar />
-            </div>
-            <div>
-               <Grid container spacing={3}>
-                  {this.state.tutorCards.map((tutor) => (
-                     <TutorSearchCard key={tutor.key} tutor={tutor} />
-                  ))}
-               </Grid>
-            </div>
+            <TutorSearchBar
+               placeholder="Wubba Lubba Dub Dub"
+               handleChange={this.handleChange}
+            />
+
+            <TutorsList tutorCards={filteredTutors} />
          </React.Fragment>
       );
    }
--- src/Components/tutors/TutorSearchBar.js
+++ src/Components/tutors/TutorSearchBar.js
@@ -1,83 +1,16 @@
 import React from "react";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import NativeSelect from "@material-ui/core/NativeSelect";
-import InputBase from "@material-ui/core/InputBase";
+import "./search-bar.css";
 
-const BootstrapInput = withStyles((theme) => ({
-   root: {
-      "label + &": {
-         marginTop: theme.spacing(3),
-      },
-   },
-   input: {
-      borderRadius: 4,
-      position: "relative",
-      backgroundColor: theme.palette.background.paper,
-      border: "1px solid #ced4da",
-      fontSize: 16,
-      padding: "10px 26px 10px 12px",
-      transition: theme.transitions.create(["border-color", "box-shadow"]),
-      // Use the system font instead of the default Roboto font.
-      fontFamily: [
-         "-apple-system",
-         "BlinkMacSystemFont",
-         '"Segoe UI"',
-         "Roboto",
-         '"Helvetica Neue"',
-         "Arial",
-         "sans-serif",
-         '"Apple Color Emoji"',
-         '"Segoe UI Emoji"',
-         '"Segoe UI Symbol"',
-      ].join(","),
-      "&:focus": {
-         borderRadius: 4,
-         borderColor: "#80bdff",
-         boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
-      },
-   },
-}))(InputBase);
+export const TutorSearchBar = ({ placeholder, handleChange }) => (
+   <div>
+      <h3>Search for tutors!</h3>
+      <input
+         className="search"
+         type="search"
+         placeholder={placeholder}
+         onChange={handleChange}
+      />
+   </div>
+);
 
-const useStyles = makeStyles((theme) => ({
-   margin: {
-      margin: theme.spacing(1),
-   },
-}));
-
-export default function CustomizedSelects() {
-   const classes = useStyles();
-   const [age, setAge] = React.useState("");
-   const handleChange = (event) => {
-      setAge(event.target.value);
-   };
-   return (
-      <div>
-         <FormControl className={classes.margin}>
-            <InputLabel htmlFor="demo-customized-textbox">
-               Tutor Search
-            </InputLabel>
-            <BootstrapInput id="demo-customized-textbox" />
-         </FormControl>
-         <FormControl className={classes.margin}>
-            <InputLabel id="demo-customized-select-label">Filter</InputLabel>
-            <Select
-               labelId="demo-customized-select-label"
-               id="demo-customized-select"
-               value={age}
-               onChange={handleChange}
-               input={<BootstrapInput />}
-            >
-               <MenuItem value="">
-                  <em>None</em>
-               </MenuItem>
-               <MenuItem value={10}>Exact</MenuItem>
-               <MenuItem value={20}>Contains</MenuItem>
-            </Select>
-         </FormControl>
-      </div>
-   );
-}
+export default TutorSearchBar;
