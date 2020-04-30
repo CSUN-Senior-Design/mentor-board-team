14b9e3d654f468d1738192aa40cced61b9917832
austinD14 <austindimalanta14@gmail.com>
2020-04-29 17:01:02 -0700

    reworking landing page, added new navbar

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react'
-import Navbar from './Navbar';
-import Sidebar from "./Sidebar";
-import logo from "./Logos/mindhive2.png";
-import Button from '@material-ui/core/Button';
 import Login from "./Login"
-
+import "./css/landingpage.css"
+import logo from "./Logos/mindhive2.png";
 export class LandingPage extends Component {
     render() {
         return (
             <React.Fragment>
-                {/* <div className = "Front-page">
-                    <img src={logo} height = "55%" alt="logo" />
-                    <h1>MentorBoard</h1>
-                    <Button variant="contained" color="primary" href="#About">
-                    About
-                    </Button>
-                    <Button variant="contained" color="primary" href="home">
-                    Login
-                    </Button>
-                </div> */}
+                <nav className = "fixed-top">
+                    <div className = "outer-container">
+                        <img
+                            src={logo}
+                            className="logo"
+                            alt="logo"
+                        />
+                        <div className= "inner-container">
+                            <div className = "brand">
+                                 MentorBoard
+                            </div>
+                            <div className = "menu">
+                                Login and signup
+                            </div>
+                        </div>
+                    </div>
+                </nav>
                 <div>
                     <Login/>
                 </div>
-                {/* <div id = "About"className = "About">
-                    <h1>about</h1>
-                </div> */}
+                <div>
+                    <Login/>
+                </div>
+                
+
             </React.Fragment>
         )
     }
