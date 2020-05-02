70bc19ad8623a00966752bf9170621cbf82dea20
jsc31994 <jsc31994@gmail.com>
2020-05-02 01:40:20 -0700

    Added the messages component

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import Header from './Header'
+import React, { Component } from "react";
+import Header from "./Header";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import MessageBox from "./Messages/MessageBox";
+
 export class Messages extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div>
-                    <Header></Header>
-                </div>
-                <div>
-                    <h2>Messages</h2>
-                </div>
-            </React.Fragment>
-        )
-    }
+   render() {
+      return (
+         <div>
+            <Header />
+            <p style={{ padding: "0 50px", fontSize: "3rem" }}>Messages</p>
+            <MessageBox />
+         </div>
+      );
+   }
 }
 
-export default Messages
+export default Messages;
